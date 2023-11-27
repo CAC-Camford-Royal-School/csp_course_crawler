7eff540d0e7ddfd97478fa77e80de647b710e22e
Hongzhe <xiehongzhe04@gmail.com>
2023-11-27 21:00:26 +0800

    add google doc download function

--- download.py
+++ download.py
@@ -1,49 +1,18 @@
-from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload
-
-SCOPES = ['https://www.googleapis.com/auth/drive']
+import re
+import requests
 
 
-def main():
-    creds = None
+def download_file_from_google_drive(link, file_path):
+    download_url = f'{link}'
+    response = requests.get(download_url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        print(f"File downloaded successfully and saved as {file_path}")
+    else:
+        print("Failed to download the file. Please check the link and try again.")
 
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('drive', 'v3', credentials=creds)
-
-        file_id = "fileID_in_Google_Drive"
-
-        data = service.files().export(fileId=file_id, mimeType="application/vnd.oasis.opendocument.text").execute()
-        if data:
-            filename = 'your-file-name.odt'
-            with open(filename, 'wb') as pdf_file:
-                pdf_file.write(data)
-
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        file = None
 
 
 if __name__ == '__main__':
-    main()
+    download_file_from_google_drive("https://docs.google.com/document/d/1Ycz0b-nlvTSMQPq4dV5MHffdJpZYw-2sPsLAVue4raE/export?format=pdf", r"C:\Users\xieho\PycharmProjects\csp_course_crawler\test.pdf")
